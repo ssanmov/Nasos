c723893072eee5edcbe1e3af1f3c1980a1fad646
Sanjarbek <sanjarbekimamov2003@gmail.com>
2022-08-30 21:08:30 +0500

    changed product serializers

--- product/serializers.py
+++ product/serializers.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 from .models import *
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['name', 'subcategory', 'description', 'price']
-
-
-class SubcategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subcategory
-        fields = ['name', 'category']
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['name']
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    category = CategorySerializer
+    class Meta:
+        model = Subcategory
+        fields = ['name', 'category']
+
+class ProductSerializer(serializers.ModelSerializer):
+    subcategory = SubcategorySerializer(many = False)
+    class Meta:
+        model = Product
+        fields = ['name', 'subcategory', 'description', 'price']
+
