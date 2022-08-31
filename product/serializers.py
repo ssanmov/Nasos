2a09ab4e3bf65de936684ffe4ead989b649068bc
Sanjarbek <sanjarbekimamov2003@gmail.com>
2022-08-31 21:42:50 +0500

    serializers changed

--- product/serializers.py
+++ product/serializers.py
@@ -14,7 +14,7 @@ class SubcategorySerializer(serializers.ModelSerializer):
         fields = ['id','name', 'image', 'category','category_id']
 
 class ProductSerializer(serializers.ModelSerializer):
-    subcategory = SubcategorySerializer(many = False)
+    subcategory = SubcategorySerializer
     class Meta:
         model = Product
         fields = ['id','name','image', 'subcategory', 'description', 'price']
