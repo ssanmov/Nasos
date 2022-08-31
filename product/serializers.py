db035c005df608ad10a947f972b853fe1e9f43d1
Sanjarbek <sanjarbekimamov2003@gmail.com>
2022-08-31 21:18:15 +0500

    changed serializers

--- product/serializers.py
+++ product/serializers.py
@@ -5,17 +5,17 @@ from .models import *
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['name']
+        fields = ['id','name']
 
 class SubcategorySerializer(serializers.ModelSerializer):
     category = CategorySerializer
     class Meta:
         model = Subcategory
-        fields = ['name', 'category']
+        fields = ['id','name', 'category','category_id']
 
 class ProductSerializer(serializers.ModelSerializer):
     subcategory = SubcategorySerializer(many = False)
     class Meta:
         model = Product
-        fields = ['name', 'subcategory', 'description', 'price']
+        fields = ['id','name','image', 'subcategory', 'description', 'price']
 
